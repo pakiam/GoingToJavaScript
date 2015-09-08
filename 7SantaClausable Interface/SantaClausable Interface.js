64618373b43353894fd5a3aa08e00a324cd2fef3
Maksim <akiam@mail.ru>
2015-09-08 23:35:45 +0300

    7 kyo task

--- 7SantaClausable Interface/SantaClausable Interface.js
+++ 7SantaClausable Interface/SantaClausable Interface.js
@@ -1,3 +1,27 @@
-/**
- * Created by Maksim on 08.09.2015.
- */
+var santa = {
+    sayHoHoHo: function () {
+        console.log('Ho Ho Ho!')
+    },
+    distributeGifts: function () {
+        console.log('Gifts for all!');
+    },
+    goDownTheChimney: function () {
+        console.log('*whoosh*');
+    }
+};
+
+var notSanta = {
+    sayHoHoHo: function () {
+        console.log('Oink Oink!')
+    }
+    // no distributeGifts() and no goDownTheChimney()
+};
+
+
+function isSantaClausable(obj) {
+    if (obj.sayHoHoHo && obj.distributeGifts && obj.goDownTheChimney) {
+
+    }
+}
+
+console.log(santa.sayHoHoHo);
