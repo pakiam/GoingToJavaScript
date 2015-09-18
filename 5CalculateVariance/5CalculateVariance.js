3160c0d51193f3a55e9c6eae557fb2ae7f21e125
Maksim <akiam@mail.ru>
2015-09-18 16:25:41 +0300

    //refactored A friend of yours is developing an application for a hotel. You should write a function that returns all names of the people on a given floor. Every floor has 6 rooms, and all rooms are numbered in a consecutive way.
    The function has the following signature:
    
    function roomMates( rooms, floor ){}
    The argument rooms holds all clients in an array, where the index (starts at 0) corresponds to the room-number (starts at 1) and holds the name of the client.
    
    floor is an integer and denotes the floor whose names need to be returned. If the floor is empty, the function roomMates should return an empty Array.
    
    rooms = [ "foo", "bar" ]
    Means that foo (index 0) stays in room #1 of the 1st floor and bar (index 1) in room #2 of the 1st floor.
    
    Empty rooms shouldn't be returned, so he can directly count the number of occupied rooms by looking at the length of the array.
    
    You have to use the filter-method of Javascript, which returns each element of the array for which the filter-method returns true. The second argument gives the index you're looking at:
    
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter

--- 5CalculateVariance/5CalculateVariance.js
+++ 5CalculateVariance/5CalculateVariance.js
@@ -1,23 +1,14 @@
 // Takes an array of numbers and returns the variance as a float.
 // The array of numbers will always have at least one element in it.
-function mean(numbers) {
-    "use strict";
-    var mean = 0;
-    for (var i = 0; i < numbers.length; i++) {
-        mean += numbers[i];
-    }
-    return mean / numbers.length;
-}
 var variance = function (numbers) {
     "use strict";
+    var mean = numbers.reduce(function (p, c) {
+            return p + c;
+        }) / numbers.length;
 
-    var sum = 0;
-    for (var i = 0; i < numbers.length; i++) {
-        sum += Math.pow(numbers[i] - mean(numbers), 2);
-
-    }
-    return sum / numbers.length;
+    return numbers.reduce(function (p, c) {
+            return p + Math.pow(c - mean, 2);
+        }, 0) / numbers.length;
 };
 
-console.log(mean([1, 2, 2, 3]));
 console.log(variance([1, 2, 2, 3]));
